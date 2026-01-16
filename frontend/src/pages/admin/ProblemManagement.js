c462a4abb51e1cf2edae6fdc0f4a100a396c9d39
Glorhop <1150595033@qq.com>
2026-01-16 12:23:22 +0800

    [FIX] Fix markdown preview rendering

--- frontend/src/pages/admin/ProblemManagement.js
+++ frontend/src/pages/admin/ProblemManagement.js
@@ -132,9 +132,10 @@ const ProblemManagement = () => {
     setIsModalOpen(true);
   };
 
-  // Image Upload Handler for SimpleMDE
-  const imageUploadOptions = useMemo(() => {
+  // SimpleMDE Options with Image Upload and Preview
+  const simpleMdeOptions = useMemo(() => {
     return {
+      spellChecker: false,
       uploadImage: true,
       imageUploadFunction: (file, onSuccess, onError) => {
         const formData = new FormData();
@@ -148,6 +149,50 @@ const ProblemManagement = () => {
             onError("Upload failed");
           });
       },
+      insertTexts: {
+        link: ["[", "](https://)"],
+        image: ["![", "](/uploads/images/)"],  
+      },
+      previewRender: (plainText) => {
+        // Simple markdown to HTML converter for preview
+        let html = plainText
+          // Headers
+          .replace(/^#### (.*$)/gim, '<h4>$1</h4>')
+          .replace(/^### (.*$)/gim, '<h3>$1</h3>')
+          .replace(/^## (.*$)/gim, '<h2>$1</h2>')
+          .replace(/^# (.*$)/gim, '<h1>$1</h1>')
+          // Bold
+          .replace(/\*\*(.*?)\*\*/gim, '<strong>$1</strong>')
+          // Italic
+          .replace(/\*(.*?)\*/gim, '<em>$1</em>')
+          // Images
+          .replace(/!\[(.*?)\]\((.*?)\)/gim, '<img alt="$1" src="$2" style="max-width: 100%;" />')
+          // Links
+          .replace(/\[(.*?)\]\((.*?)\)/gim, '<a href="$2" target="_blank">$1</a>')
+          // Line breaks
+          .replace(/\n$/gim, '<br />');
+        
+        return html;
+      },
+      toolbar: [
+        "bold",
+        "italic",
+        "heading",
+        "|",
+        "quote",
+        "code",
+        "unordered-list",
+        "ordered-list",
+        "|",
+        "link",
+        "image",
+        "|",
+        "preview",
+        "side-by-side",
+        "fullscreen",
+        "|",
+        "guide",
+      ],
     };
   }, []);
 
@@ -270,7 +315,7 @@ const ProblemManagement = () => {
             <SimpleMDE
               value={description}
               onChange={setDescription}
-              options={imageUploadOptions}
+              options={simpleMdeOptions}
             />
           </Form.Item>
         </Form>
